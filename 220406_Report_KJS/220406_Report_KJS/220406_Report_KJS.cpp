4dbc5c64f29214453d74944aea06a9188f7e57c9
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-04-06 18:35:40 +0900

    220406_Report
    .

---
+++ 220406_Report_KJS/220406_Report_KJS/220406_Report_KJS.cpp
@@ -0,0 +1,55 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    string my_name = "김재성";
+    string my_number = "010-8927-3159";
+    string e_mail = "sung9513@gmail.com";
+    string last_school = "2년대졸";
+    string my_specialty = "전자기기 비교분석";
+    string my_hobby = "게임, 음악감상";
+    string favorite_food = "소고기";
+    string hate_food = "가지";
+    string mbti = "INFJ";
+    string ideal_type = "착하고 귀여운";
+    string favorite_game = "몬스터 헌터, 파이널판타지 시리즈";
+    string hate_game = "롤, 배틀그라운드";
+    string favorite_music = "Jazz";
+    string scary = "my mother";
+    string study = "멀티 디바이스 플랫폼 메타버스 개발자 과정";
+
+    short age = 31;
+    short tall = 165;
+    short average_sleep = 6;
+    short playgame_time = 2;
+
+    float weight = 72.5f;
+
+    cout << "01. 이름\t\t: " << my_name << endl;
+    cout << "02. 연락처\t\t: " << my_number << endl;
+    cout << "03. 메일주소\t\t: " << e_mail << endl;
+    cout << "04. 나이\t\t: " << age << endl;
+    cout << "05. 키\t\t\t: " << tall << " cm" << endl;
+    cout << "06. 몸무게\t\t: " << weight<< " kg" << endl;
+    cout << "07. 최종학력\t\t: " << last_school << endl;
+    cout << "08. 특기\t\t: " << my_specialty << endl;
+    cout << "09. 취미\t\t: " << my_hobby << endl;
+    cout << "10. 좋아하는음식\t: " << favorite_food << endl;
+    cout << "11. 싫어하는음식\t: " << hate_food << endl;
+    cout << "12. MBTI\t\t: " << mbti << endl;
+    cout << "13. 이상형\t\t: " << ideal_type << endl;
+    cout << "14. 좋아하는게임\t: " << favorite_game << endl;
+    cout << "15. 싫어하는게임\t: " << hate_game << endl;
+    cout << "16. 평균수면시간\t: " << average_sleep << " 시간" << endl;
+    cout << "17. 평균게임시간\t: " << playgame_time << " 시간" << endl;
+    cout << "18. 좋아하는음악\t: " << favorite_music << endl;
+    cout << "19. 무서워하는것\t: " << scary << endl;
+    cout << "20. 배우고있는것\t: " << study << endl;
+
+}
+
